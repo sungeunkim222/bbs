6caff950144ad41aa9891214c8dd387e4ac4bf03
kimsungeun <sungeunkim222@gmail.com>
2024-11-21 15:54:07 +0900

    여백없앰, sbb nav 반영, auth 조건문반영

--- frontend/src/Components/app/Nav.js
+++ frontend/src/Components/app/Nav.js
@@ -4,68 +4,38 @@ import { Link } from "react-router-dom";
 
 function Nav() {
 
-	const { auth, setAuth } = useContext(AuthContext);
+  const { auth, setAuth } = useContext(AuthContext);
 
-	return (
-		<nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
-			<div className="container">
-
-				<div className="navbar-collapse collapse justify-content-between" id="navbar-content">
-					<ul className="navbar-nav mr-auto">
-
-						{/* 메인 화면 */}
-						<li className="nav-item">
-							<Link className="nav-link" to="/"><i className="fas fa-home"></i> Home</Link>
-						</li>
-
-						{/* 게시판 */}
-						<li className="nav-item dropdown">
-
-							<div className="nav-link dropdown-toggle" id="navbarDropdown"
-								role="button" data-toggle="dropdown" aria-haspopup="true"
-								aria-expanded="false">게시판</div>
-
-							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-								<Link className="dropdown-item" to="/bbslist">글목록</Link>
-								<Link className="dropdown-item" to="/bbswrite">글추가</Link>
-							</div>
-						</li>
-					</ul>
-					<ul className="navbar-nav ml-auto">
-
-						{							
-							(auth) ?
-								<>
-									{/* 회원 정보 */}
-									<li className="nav-item">
-										<span className="nav-link"> {auth} 님 반갑습니다 <i className="fab fa-ello"></i> &nbsp; </span>
-									</li>
-
-									{/* 로그아웃 */}
-									<li className="nav-item">
-										<Link className="nav-link" to="/logout"><i className="fas fa-sign-out-alt"></i> 로그아웃</Link>
-									</li>
-
-								</>
-								:
-								<>
-									{/* 로그인 */}
-									<li className="nav-item">
-										<Link className="nav-link" to="/login">로그인</Link>
-									</li>
-
-									{/* 회원가입 */}
-									<li className="nav-item">
-										<Link className="nav-link" to="/join">회원가입</Link>
-									</li>
-								</>
-						}
-
-					</ul>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">SBB</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              {
+                (auth) ?
+                  <>
+                    <a class="nav-link">로그아웃</a>
+                  </>
+                  :
+                  <>
+                    <a class="nav-link">로그인</a>
+                  </>
+              }
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">회원 가입</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
