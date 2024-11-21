89b832f9a45225be1ab3f1d71e4a3189c49d4378
kimsungeun <sungeunkim222@gmail.com>
2024-11-21 16:06:08 +0900

    footer 변경

--- frontend/src/Components/app/Footer.js
+++ frontend/src/Components/app/Footer.js
@@ -2,11 +2,11 @@ import "../../css/footer.css"
 
 function Footer() {
 	return (
-		<footer className="py-4 bg-dark text-light footer">
+		<footer className="py-4 bg-light footer  border-top">
           <div className="container text-center">
             <ul className="nav justify-content-center mb-3">
               <li className="nav-item">
-                <a className="nav-link text-light" href="/">Top</a>
+                <a className="nav-link" href="/">Top</a>
               </li>
             </ul>
 
